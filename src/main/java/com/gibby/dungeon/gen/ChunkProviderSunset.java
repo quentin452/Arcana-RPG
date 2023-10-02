447154092129cd1cad13009e7ae534aeeee24cd6
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-02 03:02:13 +0200

    Remove unecessary codes in ChunkProviderSunset to reduce tps lags

--- src/main/java/com/gibby/dungeon/gen/ChunkProviderSunset.java
+++ src/main/java/com/gibby/dungeon/gen/ChunkProviderSunset.java
@@ -56,69 +56,6 @@ public class ChunkProviderSunset implements IChunkProvider
         this.netherNoiseGen6 = (NoiseGeneratorOctaves)noiseGens[4];
     }
 
-    public void func_147419_a(final int p_147419_1_, final int p_147419_2_, final Block[] p_147419_3_) {
-        final byte b0 = 4;
-        final byte b2 = 32;
-        final int k = b0 + 1;
-        final byte b3 = 17;
-        final int l = b0 + 1;
-        this.noiseField = this.initializeNoiseField(this.noiseField, p_147419_1_ * b0, 15, p_147419_2_ * b0, k, b3, l);
-        final Random rand = new Random();
-        for (int i1 = 0; i1 < b0; ++i1) {
-            for (int j1 = 0; j1 < b0; ++j1) {
-                for (int k2 = 0; k2 < 16; ++k2) {
-                    final double d0 = 0.125;
-                    double d2 = this.noiseField[((i1) * l + j1) * b3 + k2];
-                    double d3 = this.noiseField[((i1) * l + j1 + 1) * b3 + k2];
-                    double d4 = this.noiseField[((i1 + 1) * l + j1) * b3 + k2];
-                    double d5 = this.noiseField[((i1 + 1) * l + j1 + 1) * b3 + k2];
-                    final double d6 = (this.noiseField[((i1) * l + j1) * b3 + k2 + 1] - d2) * d0;
-                    final double d7 = (this.noiseField[((i1) * l + j1 + 1) * b3 + k2 + 1] - d3) * d0;
-                    final double d8 = (this.noiseField[((i1 + 1) * l + j1) * b3 + k2 + 1] - d4) * d0;
-                    final double d9 = (this.noiseField[((i1 + 1) * l + j1 + 1) * b3 + k2 + 1] - d5) * d0;
-                    for (int l2 = 0; l2 < 8; ++l2) {
-                        final double d10 = 0.25;
-                        double d11 = d2;
-                        double d12 = d3;
-                        final double d13 = (d4 - d2) * d10;
-                        final double d14 = (d5 - d3) * d10;
-                        for (int i2 = 0; i2 < 4; ++i2) {
-                            int j2 = i2 + i1 * 4 << 11 | j1 * 4 << 7 | k2 * 8 + l2;
-                            final short short1 = 128;
-                            final double d15 = 0.25;
-                            double d16 = d11;
-                            final double d17 = (d12 - d11) * d15;
-                            for (int k3 = 0; k3 < 4; ++k3) {
-                                Block block;
-                                if (rand.nextInt(3) == 0) {
-                                    block = Blocks.air;
-                                }
-                                else {
-                                    block = Blocks.air;
-                                }
-                                if (k2 * 8 + l2 < b2) {
-                                    block = null;
-                                }
-                                if (d16 > 0.1) {
-                                    block = null;
-                                }
-                                p_147419_3_[j2] = block;
-                                j2 += short1;
-                                d16 += d17;
-                            }
-                            d11 += d13;
-                            d12 += d14;
-                        }
-                        d2 += d6;
-                        d3 += d7;
-                        d4 += d8;
-                        d5 += d9;
-                    }
-                }
-            }
-        }
-    }
-
     public Chunk loadChunk(final int par1, final int par2) {
         return this.provideChunk(par1, par2);
     }
@@ -126,7 +63,6 @@ public class ChunkProviderSunset implements IChunkProvider
     public Chunk provideChunk(final int par1, final int par2) {
         this.hellRNG.setSeed(par1 * 341873128712L + par2 * 132897987541L);
         final Block[] ablock = new Block[65536];
-        this.func_147419_a(par1, par2, ablock);
         final Chunk chunk = new Chunk(this.worldObj, ablock, par1, par2);
         final BiomeGenBase[] abiomegenbase = this.worldObj.getWorldChunkManager().loadBlockGeneratorData(null, par1 * 16, par2 * 16, 16, 16);
         final byte[] abyte = chunk.getBiomeArray();
@@ -135,104 +71,6 @@ public class ChunkProviderSunset implements IChunkProvider
         }
         chunk.resetRelightChecks();
         return chunk;
-    }
-
-    private double[] initializeNoiseField(double[] par1ArrayOfDouble, final int par2, final int par3, final int par4, final int par5, final int par6, final int par7) {
-        final ChunkProviderEvent.InitNoiseField event = new ChunkProviderEvent.InitNoiseField(this, par1ArrayOfDouble, par2, par3, par4, par5, par6, par7);
-        MinecraftForge.EVENT_BUS.post(event);
-        if (event.getResult() == Event.Result.DENY) {
-            return event.noisefield;
-        }
-        if (par1ArrayOfDouble == null) {
-            par1ArrayOfDouble = new double[par5 * par6 * par7];
-        }
-        final double d0 = 684.412;
-        final double d2 = 2053.236;
-        this.noiseData4 = this.netherNoiseGen6.generateNoiseOctaves(this.noiseData4, par2, par3, par4, par5, 1, par7, 1.0, 0.0, 1.0);
-        this.noiseData5 = this.netherNoiseGen7.generateNoiseOctaves(this.noiseData5, par2, par3, par4, par5, 1, par7, 100.0, 0.0, 100.0);
-        this.noiseData1 = this.netherNoiseGen3.generateNoiseOctaves(this.noiseData1, par2, par3, par4, par5, par6, par7, d0 / 80.0, d2 / 60.0, d0 / 80.0);
-        this.noiseData2 = this.netherNoiseGen1.generateNoiseOctaves(this.noiseData2, par2, par3, par4, par5, par6, par7, d0, d2, d0);
-        this.noiseData3 = this.netherNoiseGen2.generateNoiseOctaves(this.noiseData3, par2, par3, par4, par5, par6, par7, d0, d2, d0);
-        int k1 = 0;
-        int l1 = 0;
-        final double[] adouble1 = new double[par6];
-        for (int i2 = 0; i2 < par6; ++i2) {
-            adouble1[i2] = Math.cos(i2 * 3.141592653589793 * 6.0 / par6) * 2.0;
-            double d3 = i2;
-            if (i2 > par6 / 2) {
-                d3 = par6 - 1 - i2;
-            }
-            if (d3 < 4.0) {
-                d3 = 4.0 - d3;
-                adouble1[i2] -= d3 * d3 * d3 * 10.0;
-            }
-        }
-        for (int i2 = 0; i2 < par5; ++i2) {
-            for (int k2 = 0; k2 < par7; ++k2) {
-                double d4 = (this.noiseData4[l1] + 256.0) / 512.0;
-                if (d4 > 1.0) {
-                    d4 = 1.0;
-                }
-                final double d5 = 0.0;
-                double d6 = this.noiseData5[l1] / 8000.0;
-                if (d6 < 0.0) {
-                    d6 = -d6;
-                }
-                d6 = d6 * 3.0 - 3.0;
-                if (d6 < 0.0) {
-                    d6 /= 2.0;
-                    if (d6 < -1.0) {
-                        d6 = -1.0;
-                    }
-                    d6 /= 1.4;
-                    d6 /= 2.0;
-                    d4 = 0.0;
-                }
-                else {
-                    if (d6 > 1.0) {
-                        d6 = 1.0;
-                    }
-                    d6 /= 6.0;
-                }
-                d4 += 0.5;
-                d6 = d6 * par6 / 16.0;
-                ++l1;
-                for (int j2 = 0; j2 < par6; ++j2) {
-                    double d7;
-                    final double d8 = adouble1[j2];
-                    final double d9 = this.noiseData2[k1] / 512.0;
-                    final double d10 = this.noiseData3[k1] / 512.0;
-                    final double d11 = (this.noiseData1[k1] / 10.0 + 1.0) / 2.0;
-                    if (d11 < 0.0) {
-                        d7 = d9;
-                    }
-                    else if (d11 > 1.0) {
-                        d7 = d10;
-                    }
-                    else {
-                        d7 = d9 + (d10 - d9) * d11;
-                    }
-                    d7 -= d8;
-                    if (j2 > par6 - 4) {
-                        final double d12 = (j2 - (par6 - 4)) / 3.0f;
-                        d7 = d7 * (1.0 - d12) + -10.0 * d12;
-                    }
-                    if (j2 < d5) {
-                        double d12 = (d5 - j2) / 4.0;
-                        if (d12 < 0.0) {
-                            d12 = 0.0;
-                        }
-                        if (d12 > 1.0) {
-                            d12 = 1.0;
-                        }
-                        d7 = d7 * (1.0 - d12) + -10.0 * d12;
-                    }
-                    par1ArrayOfDouble[k1] = d7;
-                    ++k1;
-                }
-            }
-        }
-        return par1ArrayOfDouble;
     }
 
     public boolean chunkExists(final int par1, final int par2) {
